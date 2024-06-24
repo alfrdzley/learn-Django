451caf293ec24be22650927d702538f5c930936e
Alfrdzley <2311102441242@umkt.ac.id>
2024-06-24 17:51:50 +0800

    feat: Add static stylesheet to index.html for polls

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone 
+from django.test import TestCase
 
 from .models import Question, Choice
 
