65a8a88e18eca6b5bdbf5e58d328efaa6dcd3840
Alfrdzley <2311102441242@umkt.ac.id>
2024-06-26 19:19:08 +0800

    feat: Update Django templates and admin configuration for the "polls" app

--- polls/views.py
+++ polls/views.py
@@ -29,12 +29,22 @@ class DetailView(generic.DetailView):
         return Question.objects.filter(pub_date__lte=timezone.now())
     
 class QuestionDetailViewTests(TestCase):
+    def create_question(self, question_text, days):
+        """
+        Create a question with the given `question_text` and published
+        the given number of `days` offset to now (negative for questions
+        published in the past, positive for questions that have yet
+        to be published).
+        """
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
+
     def test_future_question(self):
         """
         The detail view of a question with a pub_date in the future
         returns a 404 not found.
         """
-        future_question = create_question(question_text="Future question.", days=5)
+        future_question = self.create_question(question_text="Future question.", days=5)
         url = reverse("polls:detail", args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
@@ -44,7 +54,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in the past
         displays the question's text.
         """
-        past_question = create_question(question_text="Past Question.", days=-5)
+        past_question = self.create_question(question_text="Past Question.", days=-5)
         url = reverse("polls:detail", args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
