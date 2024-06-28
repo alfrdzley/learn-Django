57d5ae44dc4beca66a573a129c89d81bd080cd3d
Alfrdzley <2311102441242@umkt.ac.id>
2024-06-28 13:27:52 +0800

    feat: Add index view to display a list of polls

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from educative import views
 
 urlpatterns = [
+    path('',views.index,name="index"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
+
