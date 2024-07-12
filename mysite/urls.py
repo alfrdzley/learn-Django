8b4dea0283357d2112935913ce903765195245a1
SYERA AL-FARIDZI <2311102441242@umkt.ac.id>
2024-07-12 15:38:29 +0800

    chore: Remove unnecessary files and code for the "educative" app

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from educative import views
 
 urlpatterns = [
-    path('',views.index,name="index"),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
